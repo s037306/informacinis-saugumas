b548d7e7fb897207b8ff4d4fc3886f650f4d1bea
s037306 <76158948+s037306@users.noreply.github.com>
2021-08-29 22:55:30 +0300

    Add files via upload

---
+++ RSA algoritmas/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace RSA_algoritmas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            this.qTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.cleanButton = new System.Windows.Forms.Button();
+            this.DecryptFromText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RSA ALGORITMAS";
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Location = new System.Drawing.Point(141, 51);
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pTextBox.TabIndex = 1;
+            // 
+            // qTextBox
+            // 
+            this.qTextBox.Location = new System.Drawing.Point(141, 77);
+            this.qTextBox.Name = "qTextBox";
+            this.qTextBox.Size = new System.Drawing.Size(100, 20);
+            this.qTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "p:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "q:";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(81, 155);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(249, 20);
+            this.xTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(317, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Norimas šifruoti tekstas/dešifruotas tekstas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(334, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Norimas dešifruoti tekstas/užšifruotas tekstas:";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(71, 253);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(249, 20);
+            this.yTextBox.TabIndex = 7;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(141, 181);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 9;
+            this.encryptButton.Text = "Šifruoti";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(81, 279);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 25);
+            this.decryptButton.TabIndex = 10;
+            this.decryptButton.Text = "Dešifruoti";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // cleanButton
+            // 
+            this.cleanButton.Location = new System.Drawing.Point(141, 316);
+            this.cleanButton.Name = "cleanButton";
+            this.cleanButton.Size = new System.Drawing.Size(75, 43);
+            this.cleanButton.TabIndex = 11;
+            this.cleanButton.Text = "Valyti langelius";
+            this.cleanButton.UseVisualStyleBackColor = true;
+            this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+            // 
+            // DecryptFromText
+            // 
+            this.DecryptFromText.Location = new System.Drawing.Point(186, 279);
+            this.DecryptFromText.Name = "DecryptFromText";
+            this.DecryptFromText.Size = new System.Drawing.Size(123, 25);
+            this.DecryptFromText.TabIndex = 12;
+            this.DecryptFromText.Text = "Įkelti tekstą iš failo";
+            this.DecryptFromText.UseVisualStyleBackColor = true;
+            this.DecryptFromText.Click += new System.EventHandler(this.DecryptFromText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(382, 371);
+            this.Controls.Add(this.DecryptFromText);
+            this.Controls.Add(this.cleanButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.qTextBox);
+            this.Controls.Add(this.pTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pTextBox;
+        private System.Windows.Forms.TextBox qTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button cleanButton;
+        private System.Windows.Forms.Button DecryptFromText;
+    }
+}
+
---
+++ RSA algoritmas/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.IO;
+
+namespace RSA_algoritmas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = xTextBox.Text;
+                int p = Int32.Parse(pTextBox.Text);
+                int q = Int32.Parse(qTextBox.Text);
+                Validation(p, q);
+                int n = p * q;
+
+                File.WriteAllText(@"C:\Users\Kakarotas\Desktop\Atsiskaitymai\Informacijos saugumas\RSA-algoritmas-master\RSA-algoritmas-master\file.txt", n.ToString());
+                StreamWriter streamWriter = new StreamWriter(@"C:\Users\Kakarotas\Desktop\Atsiskaitymai\Informacijos saugumas\RSA-algoritmas-master\RSA-algoritmas-master\file.txt", true);
+
+                int fn = (p - 1) * (q - 1);
+                int exponent = GetEValue(fn);
+                IntWriteToFile(exponent, streamWriter);
+                yTextBox.Text = Encryption(exponent, n, CreateAsciiDecimals(text), streamWriter);
+                Console.WriteLine("n " + n);
+                Console.WriteLine("fn " + fn);
+                Console.WriteLine("e " + exponent);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+        private void IntWriteToFile(int text, StreamWriter streamWriter)
+        {
+            streamWriter.WriteLine("\n" + text.ToString());
+        }
+        private void StringWriteToFile(string text, StreamWriter streamWriter)
+        {
+            streamWriter.WriteLine(text);
+            streamWriter.Close();
+        }
+        private void Validation(int p, int q)
+        {
+            for (int i = 2; i < p; i++)
+                if (p % i == 0 && i != p)
+                    throw new Exception("p must be a prime number");
+
+            for (int i = 2; i < q; i++)
+                if (q % i == 0 && i != q)
+                    throw new Exception("q must be a prime number");
+            if (p < 10 || q < 10)
+                throw new Exception("q and p cannot have a value below 10");
+        }
+        private int GetEValue(int fn)
+        {
+            for (int e = 2; e < fn; e++)
+                if (GCD(e, fn) == 1)
+                {
+                    int exponent = e;
+                    if (exponent > 1)
+                        return exponent;
+                }
+            throw new Exception("e not found");
+        }
+        private int GCD(int a, int b)
+        {
+            int temp = 0;
+            while (b != 0)
+            {
+                temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+        private List<int> CreateAsciiDecimals(string text)
+        {
+            List<int> AsciiDecimals = new List<int>();
+            foreach (char a in text)
+                AsciiDecimals.Add(a);
+            return AsciiDecimals;
+        }
+        private string Encryption(int exponent, int n, List<int> AsciiDecimals, StreamWriter streamWriter) 
+        {
+            string encryptedText = "";
+            foreach (char a in AsciiDecimals)
+            { 
+                BigInteger poweredByE = BigInteger.Pow(a, exponent);
+                BigInteger encryptedChar = poweredByE % n;
+                encryptedText += (char)(encryptedChar);
+            }
+            StringWriteToFile(encryptedText, streamWriter);
+            return encryptedText;
+        }
+         
+        private int privateKeyValue (int fn, int exponent)
+        {
+            int d = 1;
+            while (d * exponent % fn != 1)
+            {
+                d++;
+            }
+            return d;
+        }
+
+        private string Decryption(int d, int n, List<int> AsciiDecimals)
+        {
+            string decryptedText = "";
+            foreach (char a in AsciiDecimals)
+            {
+                BigInteger poweredByD = BigInteger.Pow(a, d);
+                BigInteger decryptedChar = poweredByD % n;
+                decryptedText += (char)(decryptedChar);
+            }
+            return decryptedText;
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            string text = yTextBox.Text;
+            int p = Int32.Parse(pTextBox.Text);
+            int q = Int32.Parse(qTextBox.Text);
+            Validation(p, q);
+            int n = p * q;
+            int fn = (p - 1) * (q - 1);
+            int exponent = GetEValue(fn);
+            int d = privateKeyValue(fn, exponent);
+            xTextBox.Text = Decryption(d, n, CreateAsciiDecimals(text));
+            Console.WriteLine("d " + d);
+        }
+
+       
+
+        private void DecryptFromText_Click(object sender, EventArgs e)
+        {
+            string line;
+            List<string> parts = new List<string>();
+            StreamReader file = new StreamReader(@"C:\Users\Kakarotas\Desktop\Atsiskaitymai\Informacijos saugumas\RSA-algoritmas-master\RSA-algoritmas-master\file.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                parts.Add(line);
+            }
+
+
+            int n = Int32.Parse(parts[0]);
+            int p = 2;
+            int q = 0;
+            while (n % p > 0) 
+            {
+                p++; 
+            }
+            q = n / p;
+
+            pTextBox.Text = p.ToString();
+            qTextBox.Text = q.ToString();
+            yTextBox.Text = parts[2];
+
+        }
+
+        private void cleanButton_Click(object sender, EventArgs e)
+        {
+            pTextBox.Text = "";
+            qTextBox.Text = "";
+            xTextBox.Text = "";
+            yTextBox.Text = "";
+        }
+    }
+}
